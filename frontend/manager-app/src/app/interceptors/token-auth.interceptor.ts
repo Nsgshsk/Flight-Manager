b6616006db82b1243900b3a17f65dc80226a2bb4
Lubomir Topalski <lubomirtopalski@gmail.com>
2024-04-19 12:03:08 +0300

    working on interceptor

--- frontend/manager-app/src/app/interceptors/token-auth.interceptor.ts
+++ frontend/manager-app/src/app/interceptors/token-auth.interceptor.ts
@@ -4,8 +4,10 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
+  HttpErrorResponse,
+  HttpResponse,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, switchMap, throwError } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { AuthService } from '../services/authentication/auth.service';
 import { TokenStorageService } from '../services/authentication/token-storage.service';
@@ -14,6 +16,8 @@ const apiPaths = []
 
 @Injectable()
 export class TokenAuthInterceptor implements HttpInterceptor {
+  private isRefreshing = false;
+
   constructor(
     private tokenStorage: TokenStorageService,
     private auth: AuthService
@@ -35,6 +39,48 @@ export class TokenAuthInterceptor implements HttpInterceptor {
       });
     }
 
-    return next.handle(request);
+    return next.handle(request).pipe(catchError(error => {
+      if (error instanceof HttpErrorResponse && !request.url.includes('auth/token/') && error.status === 401) {
+        return this.handle401Error(request, next);
+      }
+
+      return throwError(() => error);
+    })) as Observable<HttpEvent<any>>;
+  }
+
+  private handle401Error(request: HttpRequest<any>, next: HttpHandler) {
+    if (!this.isRefreshing) {
+      this.isRefreshing = true;
+
+      if (!this.auth.isTokenRefreshExpired()){
+        this.auth.refreshToken();
+        if (!this.auth.isTokenAccessExpired()){
+          const token = this.tokenStorage.getTokenAccess();
+          request = request.clone({
+            setHeaders: { Authorization: `Bearer ${token}` },
+          });
+          return next.handle(request);
+        }
+      }
+      return throwError(() => new Error())
+
+
+        return this.auth.refreshToken().pipe(
+          switchMap((token: any) => {
+            this.isRefreshing = false;
+
+            this.tokenService.saveToken(token.accessToken);
+            this.refreshTokenSubject.next(token.accessToken);
+            
+            return next.handle(this.addTokenHeader(request, token.accessToken));
+          }),
+          catchError((err) => {
+            this.isRefreshing = false;
+            
+            this.auth.logoutToken();
+            return throwError(() => err);
+          })
+        );
+    }
   }
 }
